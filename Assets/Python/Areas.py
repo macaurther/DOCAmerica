6d049824cc3bcf6afd714757d0a609eae493cf6a
macaurther <macaurther@gmail.com>
2022-09-27 17:05:51 -0400

    Base pass on terrain and features of map

--- Assets/Python/Areas.py
+++ Assets/Python/Areas.py
@@ -6,20 +6,20 @@ lPeakExceptions = []
 ### Capitals ###
 
 dCapitals = CivDict({
-iMaya :			(36, 49), # Tikal
-iInca :			(36, 49), # Cuzco
-iAztecs :		(36, 49), # Tenochtitlan
-iSpain :		(36, 49), # Santo Domingo
-iPortugal :		(36, 49), # Sao Paulo
-iEngland :		(36, 49), # Jamestown
-iFrance :		(36, 49), # Quebec
-iNetherlands :	(36, 49), # New Amsterdam
-iAmerica :		(36, 49), # Washington
-iArgentina :	(36, 49), # Buenos Aires
-iMexico :		(36, 49), # Mexico City
-iColombia :		(36, 49), # Bogota
-iBrazil :		(36, 49), # Rio de Janeiro
-iCanada :		(36, 49), # Montreal
+iMaya :			(36, 58), # Tikal
+iInca :			(49, 36), # Cuzco
+iAztecs :		(30, 60), # Tenochtitlan
+iSpain :		(54, 59), # Santo Domingo
+iPortugal :		(67, 27), # Sao Paulo
+iEngland :		(50, 77), # Jamestown
+iFrance :		(58, 89), # Quebec
+iNetherlands :	(56, 81), # New Amsterdam
+iAmerica :		(51, 79), # Washington
+iArgentina :	(57, 16), # Buenos Aires
+iMexico :		(30, 60), # Mexico City
+iColombia :		(48, 49), # Bogota
+iBrazil :		(70, 28), # Rio de Janeiro
+iCanada :		(52, 87), # Ottawa
 })
 
 dPeriodCapitals = {
