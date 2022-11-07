b6f61ae8148be16c3dc63cc8864333798b7b015a
macaurther <macaurther@gmail.com>
2022-11-07 13:11:34 -0500

    Made Brazilian coast more interesting

--- Assets/Python/Areas.py
+++ Assets/Python/Areas.py
@@ -20,7 +20,7 @@ iInca :			(49, 36), # Cuzco
 iAztecs :		(30, 60), # Tenochtitlan
 iSpain :		(55, 59), # Santo Domingo
 iIroquois :		(51, 84), # Onondaga
-iPortugal :		(67, 27), # Sao Paulo
+iPortugal :		(67, 26), # Sao Paulo
 iEngland :		(50, 77), # Jamestown
 iFrance :		(58, 89), # Quebec
 iNetherlands :	(56, 81), # New Amsterdam
