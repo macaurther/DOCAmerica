8f96749fc72fd054c8f1728b34645a78f80798ba
macaurther <macaurther@gmail.com>
2023-01-20 16:03:17 -0500

    Removed unwanted events, added a few events, switched around a few civics

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -581,8 +581,8 @@ iNumBuildingsPlague = iNumBuildings+1
 
 #Civics
 iNumCivics = 42
-(iChiefdom, iDespotism, iEmpire, iColony, iMonarchy, iConfederacy, iRepublic,
-iAuthority, iCouncil, iCommonLaw, iViceroyality, iFederalism, iSelfDetermination, iDemocracy,
+(iChiefdom, iCouncil, iDespotism, iMonarchy, iViceroyality, iSelfDetermination, iDemocracy,
+iAuthority, iEmpire, iColony, iCommonLaw, iConfederacy, iFederalism, iRepublic,
 iTraditionalism, iSpecialization, iSlavery, iIndenturedServitude, iIndustrialism, iMigrantWorkers, iAutomation,
 iReciprocity, iMerchantTrade, iMercantilism, iAgrarianism, iFreeEnterprise, iConsumerism, iPublicWelfare,
 iAnimism, iCasteSystem, iIsolationism, iHaven, iProfiteering, iOpportunity, iMulticulturalism,
